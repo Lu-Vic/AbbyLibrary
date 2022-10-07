9708c268be73c93ae76e201510f7d496a258aec7
Lu-Vic <victorluizfr@hotmail.com>
2022-10-07 11:59:33 -0300

    first mods

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TopoComponent } from './topo/topo.component';
+import { HomeComponent } from './home/home.component';
+import { SobreMimComponent } from './sobre-mim/sobre-mim.component';
+import { ObrasComponent } from './obras/obras.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopoComponent,
+    HomeComponent,
+    SobreMimComponent,
+    ObrasComponent
   ],
   imports: [
     BrowserModule
